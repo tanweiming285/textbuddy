1e9edb8a9cf9605b14cc4bf867265a0ccaa1fadd
tanweiming285 <tanweiming285@gmail.com>
2015-09-15 19:22:10 +0800

    created junit test case

---
+++ TextBuddyTest.java
@@ -0,0 +1,73 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TextBuddyTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFetchFromFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessInput() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStopProgram() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCommand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArgument() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewEntry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteEntry() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearAll() {
+		fail("Not yet implemented");
+	}
+
+}
